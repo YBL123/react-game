58d3bc273f4786101a1dc61d55f705540d3c40aa
YardenLawson <yardenlawson@gmail.com>
2020-07-03 13:14:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-// import { render } from 'node-sass'
+
+import menuArt from './assets/menu-art.png'
+import gameOverImage from './assets/game-over-art.png'
 
 class App extends React.Component {
   state = {
@@ -53,11 +55,83 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="game-wrapper">
-        <div className="grid">
-          <div className="grid2"></div>
-        </div>
+<body>
+  <div className="container">
+
+    <div className="game-menu">
+      <div className="game-title">
+        <h1 className="game-title-flareon-h1">Flareon</h1>
+        <h2 className="game-title-flareon-h2">Frogger</h2>
       </div>
+      <div><img className="game-menu-image" src={menuArt} alt="flareon"/></div>
+      <div className="game-instructions">
+        <p>Get all four Flareons to their beach hangout! <br/> Watch out for the Lugias! use floats to get across the water! <br/> Collect Gems!</p>
+      </div>
+      <div className="difficulty">
+        <input type="button" className="difficulty-button active-button" value="easy"/>
+        <input type="button" className="difficulty-button" value="medium"/>
+        <input type="button" className="difficulty-button" value="hard"/>
+      </div>
+      <div className="start-game-button">
+        <button id="start">Start!</button>
+      </div>
+    </div>
+
+    <div className="game-over">
+      <div>
+        <h1>Game Over</h1>
+      </div>
+      <div className="final-score">
+        You Scored
+      </div>
+      <div><img className="game-menu-image" src={gameOverImage} alt="lugia"/></div>
+      <div>
+        <button className="play-again-button">Play Again</button>
+      </div>
+    </div>
+
+    <div className="game-won">
+      <div>
+        <h1>Congratulations! You Won!</h1>
+      </div>
+      <div className="final-score2">
+        You Scored
+      </div>
+      <div><img className="game-menu-image" src="./assets/game-won-art.png" alt="happy-flareon"/></div>
+      <div className="score-input">
+        Enter name For High Score Chart!
+        <input type="text"/>
+      </div> 
+      <div>
+        <button className="play-again-button2">Play Again</button>
+      </div>
+    </div>
+
+    <div className="game-wrapper">      
+      <div className="controls-wrapper">
+        <p>Lives: <span id="player-lives" value="easy">5</span></p>
+        <p>Score: <span id="score-display">0</span></p>
+      </div>
+      <div className="grid">
+      </div>
+      <div className="reset-button">
+        <button id="reset" className="reset">reset</button>
+      </div>
+    </div>
+
+      <div className="high-score-table">
+      <div>
+        <h1>High Score</h1>
+      </div>
+      <div className="score-table">
+      </div>
+    </div> 
+
+  </div>
+  
+  <audio></audio>
+  
+</body>
     )
   }
 }
